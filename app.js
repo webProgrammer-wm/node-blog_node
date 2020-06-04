7d3ab6b01a22805972c8b8a34d4c429095fe0708
allen <1603657728@qq.com>
2020-06-04 17:54:39 +0800

    3 处理HTTP请求的综合示例

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 const http = require('http')
 const queryString = require('querystring')
-//
-// const server = http.createServer((req, res) => {
-//     console.log(req.method) // GET
-//     const url = req.url
-//     console.log(url)
-//
-//     req.query = queryString.parse(url.split('?')[1])
-//     console.log(req.query)
-//
-//     res.end(
-//         JSON.stringify(req.query)
-//     )
-// })
-//
-// server.listen(8000)
-// console.log('ok')
-
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        // req 数据格式
-        console.log('req content-type', req.headers["content-type"])
+    const method = req.method
+    const url = req.url
+    const path = url.split('?')[0]
+    const query = queryString.parse(url.split('?')[1])
 
-        // 接收数据
+    // 设置返回格式为 JSON
+    res.setHeader('Content-type', 'application/json')
+
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+
+    // 返回
+    if (method === 'GET') {
+        res.end(
+            JSON.stringify(resData)
+        )
+    }
+
+    if (method === 'POST') {
         let postData = ''
         req.on('data', chunk => {
             postData += chunk.toString()
         })
         req.on('end', () => {
-            console.log('postData', postData)
-            res.end('hello world')
+            resData.postData = postData
+            // 返回
+            res.end(
+                JSON.stringify(resData)
+            )
         })
     }
 })
