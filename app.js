804f5617b9b1cf9929b13856907a6f63abfac48d
allen <1603657728@qq.com>
2020-06-04 17:43:23 +0800

    2 处理POST请求

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const http = require('http')
 const queryString = require('querystring')
+//
+// const server = http.createServer((req, res) => {
+//     console.log(req.method) // GET
+//     const url = req.url
+//     console.log(url)
+//
+//     req.query = queryString.parse(url.split('?')[1])
+//     console.log(req.query)
+//
+//     res.end(
+//         JSON.stringify(req.query)
+//     )
+// })
+//
+// server.listen(8000)
+// console.log('ok')
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.method) // GET
-    const url = req.url
-    console.log(url)
+    if (req.method === 'POST') {
+        // req 数据格式
+        console.log('req content-type', req.headers["content-type"])
 
-    req.query = queryString.parse(url.split('?')[1])
-    console.log(req.query)
-
-    res.end(
-        JSON.stringify(req.query)
-    )
+        // 接收数据
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            console.log('postData', postData)
+            res.end('hello world')
+        })
+    }
 })
 
 server.listen(8000)
